e04265a7811d71b2422b5b4806bba7528f73e86a
Mico Milic <milicftn@gmail.com>
2022-04-10 15:18:15 +0200

    Spring init

---
+++ Back-end/src/test/java/mrsisa/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mrsisa.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

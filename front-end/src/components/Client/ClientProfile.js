3ed429fb921958c1cc2defbdd421faf3728de55e
ivana.ste <istevanovic3112@gmail.com>
2022-04-19 01:14:21 +0200

    #3.4.1_1 Proširiti UserProfile komponentu dodavanjem mogućnosti ažuriranja svakog podatka putem dugmeta

--- front-end/src/components/Client/ClientProfile.js
+++ front-end/src/components/Client/ClientProfile.js
@@ -3,6 +3,18 @@ import "./ClientProfile.css"
 import ClientService from '../../services/ClientService'
 
 export default function ClientProfile() {
+    const [editClicked, setEditClicked] = useState(
+        {
+            usernameButton: false,
+            nameButton: false,
+            surnameButton: false,
+            emailButton: false,
+            phoneNumberButton: false,
+            streetButton: false,
+            cityButton: false,
+            stateButton: false
+        }   
+    )
     const [client, setClient] = useState(
         {
             username: "",
@@ -21,6 +33,44 @@ export default function ClientProfile() {
         }
     )
 
+    function updateClient(event) {
+        const {name, value} = event.target
+        let address = false
+        if (['street', 'city', 'state'].includes(name)) {
+            address = true
+        }
+        setClient(prevClientData => {
+            if (address) {
+                return {
+                    ...prevClientData,
+                    "address": {
+                        ...prevClientData.address,
+                        [name] : value
+                    }
+                }
+            } else {
+                return {
+                    ...prevClientData,
+                    [name] : value
+                }   
+            }
+        })
+    }
+
+
+    function toggleShown(event){
+        event.preventDefault()
+        const {id} = event.target
+        setEditClicked(prevShown => {
+            return {
+                ...prevShown,
+                [id]: !editClicked[id]
+            }
+        })
+        ClientService.updateClient(client)
+    }
+    
+
     useEffect(() => {
         ClientService.getClient().then((response) => {
             setClient(response.data) 
@@ -33,83 +83,123 @@ export default function ClientProfile() {
             <h2 className="userProfileHeader"> Personal details</h2>    
             <form className="userProfileForm">
             <label className="userLabel"> Username </label>
+            {editClicked.usernameButton ? 
                 <input className="textBox"
                 type="text"
+                onChange={updateClient}
                 name="username"
                 value={client.username}
                 />
+            : 
+                <label className='userLabel'> {client.username}</label>
+            }
+            <button id="usernameButton" onClick={toggleShown}> {editClicked.usernameButton? "Save" : "Edit"}</button>
             <br></br>
             <br></br>
 
             <label className="userLabel"> First Name </label>
-           
+            {editClicked.nameButton ? 
                 <input className="textBox"
                 type="text"
+                onChange={updateClient}
                 name="name"
                 value={client.name}
             />
+            : 
+                <label className='userLabel'> {client.name}</label>
+            }
+            <button id="nameButton" onClick={toggleShown}> {editClicked.nameButton? "Save" : "Edit"}</button>
             <br></br>
             <br></br>
 
             <label className="userLabel"> Last Name </label>
-    
+            {editClicked.surnameButton ? 
                 <input className="textBox"
                 type="text"
+                onChange={updateClient}
                 name="surname"
                 value={client.surname}
             />
-           
+            : 
+                <label className='userLabel'> {client.surname}</label>
+            }
+            <button id="surnameButton" onClick={toggleShown}> {editClicked.surnameButton? "Save" : "Edit"}</button>
             <br></br>
             <br></br>
 
             <label className="userLabel"> Email </label>
-        
+            {editClicked.emailButton ? 
                 <input className="textBox"
                 type="text"
+                onChange={updateClient}
                 name="email"
                 value={client.email}
             />
+            : 
+                <label className='userLabel'> {client.email}</label>
+            }
+            <button id="emailButton" onClick={toggleShown}> {editClicked.emailButton? "Save" : "Edit"}</button>
             <br></br>
             <br></br>
 
 
             <label className="userLabel"> PhoneNumber </label>
-
+            {editClicked.phoneNumberButton? 
                 <input className="textBox"
                 type="text"
+                onChange={updateClient}
                 name="phoneNumber"
                 value={client.phoneNumber}
             />
-        
+            : 
+                <label className='userLabel'> {client.phoneNumber}</label>
+            }
+            <button id="phoneNumberButton" onClick={toggleShown}> {editClicked.phoneNumberButton? "Save" : "Edit"}</button>
             <br></br>
             <br></br>
 
             <label className="userLabel"> Street </label>
-
+            {editClicked.streetButton? 
                 <input className="textBox"
                 type="text"
+                onChange={updateClient}
                 name="street"
                 value={client.address.street}
             />
-           
+            : 
+                <label className='userLabel'> {client.address.street}</label>
+            }
+            <button id="streetButton" onClick={toggleShown}> {editClicked.streetButton? "Save" : "Edit"}</button>
             <br></br>
             <br></br>
 
             <label className="userLabel"> City </label>
+            {editClicked.cityButton? 
                 <input className="textBox"
                 type="text"
+                onChange={updateClient}
                 name="city"
                 value={client.address.city}
-           />
+            />
+            : 
+                <label className='userLabel'> {client.address.city}</label>
+            }
+            <button id="cityButton" onClick={toggleShown}> {editClicked.cityButton? "Save" : "Edit"}</button>
             <br></br>
             <br></br>
             
             <label className="userLabel"> State </label>
+            {editClicked.stateButton? 
                 <input className="textBox"
                 type="text"
+                onChange={updateClient}
                 name="state"
                 value={client.address.state}
             />
+            : 
+                <label className='userLabel'> {client.address.state}</label>
+            }
+            <button id="stateButton" onClick={toggleShown}> {editClicked.stateButton? "Save" : "Edit"}</button>
             </form>
         </div>
     )

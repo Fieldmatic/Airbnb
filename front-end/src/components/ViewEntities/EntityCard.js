679f09fcff7c2b83617d662e9e9b3d53cc20e98b
ivana.ste <stevanovic.sw69.2019@uns.ac.rs>
2022-05-03 15:44:58 +0200

    #3.12_2 Kreirati komponentu EntityCard koja sadrži glavne informacije o vikendici, brodu ili avanturi

--- front-end/src/components/ViewEntities/EntityCard.js
+++ front-end/src/components/ViewEntities/EntityCard.js
@@ -1,15 +1,27 @@
 import React from "react"
 import CottageService from "../../services/CottageService"
 import LocationOnIcon from '@mui/icons-material/LocationOn'
+import BoatService from "../../services/BoatService"
+import AdventureService from "../../services/AdventureService"
 
 export default function EntityCard(props) {
     const [reviewsNumber, setReviewsNumber] = React.useState(0)
 
     React.useEffect(() => {
-                CottageService.getNumberOfReviews().then((response) => {
-                    setReviewsNumber(response.data) 
-           //         console.log(response.data)
-                })
+        if (props.entityType === "cottage") {
+            CottageService.getNumberOfCottageReviews(props.id).then((response) => {
+                setReviewsNumber(response.data) 
+            })
+        } else if (props.entityType === "boat") {
+            BoatService.getNumberOfBoatReviews(props.id).then((response) => {
+                setReviewsNumber(response.data) 
+            })
+        //avanture
+        } else {
+            AdventureService.getNumberOfAdventureReviews(props.id).then((response) => {
+                setReviewsNumber(response.data) 
+            })
+        }
    }, [])
 
     return(
@@ -17,10 +29,10 @@ export default function EntityCard(props) {
             <img src={`../images/user.png`} className="entityCardImage" alt="user"/>
             <h1 className="entityCardName">{props.name}</h1>
             <LocationOnIcon className="addressLogo"></LocationOnIcon>            
-            <p className="entityCardAddress"> {props.address}</p>
+            <p className="entityCardAddress"> {props.address.city}, {props.address.state}</p>
             <p className="entityCardDescription">{props.promotionalDescription}</p> 
+            <p className="entityCardReviews">{reviewsNumber} reviews</p>
             <div className="entityCardGrade"> 
-               <span> {reviewsNumber}</span>
                 <span> Rating: </span>
                 <span className="entityCardGrade"> {props.rating}</span>
             </div>

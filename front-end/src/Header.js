e653c4a8d1031eb158d1b5e7c3d824e0869f727a
Mico Milic <milicftn@gmail.com>
2022-05-23 19:17:47 +0200

    Dinamicna izmena headera na osnovu role-a

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -7,16 +7,26 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Avatar } from '@mui/material';
 import { Link } from 'react-router-dom';
 import inMemoryJwt from './services/inMemoryJwtService';
+import LoginRegisterService from './services/LoginRegisterService';
 
 export default function Header(){
 
     const [isUserLogged, setIsUserLogged] = useState(false);
+    const [role, setRole] = useState(null);
     
     useEffect(() => {
         inMemoryJwt.setToken(localStorage.getItem("user"))
         inMemoryJwt.setExpiresIn(localStorage.getItem("expiration"))
         if ((inMemoryJwt.getToken()) !== null) setIsUserLogged(true);
         else setIsUserLogged(false)
+        if (isUserLogged){
+            LoginRegisterService.getUserRole().then(response => {
+                if (response.data === "ROLE_COTTAGE_OWNER") setRole("COTTAGE_OWNER")
+                else if (response.data === "ROLE_BOAT_OWNER") setRole("BOAT_OWNER")
+                else if (response.data ==="ROLE_INSTRUCTOR") setRole("INSTRUCTOR")
+                else setRole("CLIENT")
+            })
+        }
     }, [isUserLogged]);
 
     const logoutHandler = event => {
@@ -24,7 +34,55 @@ export default function Header(){
         inMemoryJwt.deleteExpiration()
         inMemoryJwt.deleteToken()
         setIsUserLogged(false)
+        setRole(null)
     };
+
+    function getUnauthorizedOptions(){
+        return (
+            <div className='header__links'>
+                <Link to={'/showEntities'} style={{textDecoration: 'none', color:'black'}}>View Offers</Link>
+            </div>
+        )
+    }
+
+    function getClientOptions(){
+        return (
+            <div className='header__links'>
+                <Link to={'/editProfile'} style={{textDecoration: 'none', color:'black'}}>Edit Your Profile</Link>
+                <Link to={'/showEntities'} style={{textDecoration: 'none', color:'black'}}>View Offers</Link>
+            </div>
+        )
+    }
+
+    function getCottageOwnerOptions(){
+        return (
+            <div className='header__links'>
+                <Link to={'/addCottage'} style={{textDecoration: 'none', color:'black'}}>Add Cottage</Link>
+                <Link to={'/editProfile'} style={{textDecoration: 'none', color:'black'}}>Edit Your Profile</Link>
+                <Link to={'/viewHostEntities'} style={{textDecoration: 'none', color:'black'}}>View your entities</Link>
+            </div>
+        )
+    }
+
+    function getBoatOwnerOptions(){
+        return (
+            <div className='header__links'>
+                <Link to={'/addBoat'} style={{textDecoration: 'none', color:'black'}}>Add Boat</Link>
+                <Link to={'/editProfile'} style={{textDecoration: 'none', color:'black'}}>Edit Your Profile</Link>
+                <Link to={'/viewHostEntities'} style={{textDecoration: 'none', color:'black'}}>View your entities</Link>
+            </div>
+        )
+    }
+
+    function getInstructorOptions(){
+        return (
+            <div className='header__links'>
+                <Link to={'/addAdventure'} style={{textDecoration: 'none', color:'black'}}>Add Adventure</Link>
+                <Link to={'/editProfile'} style={{textDecoration: 'none', color:'black'}}>Edit Your Profile</Link>
+                <Link to={'/viewHostEntities'} style={{textDecoration: 'none', color:'black'}}>View your entities</Link>
+            </div>
+        )
+    }
 
     return (
         <div className = 'header'>
@@ -34,16 +92,13 @@ export default function Header(){
                 alt = ""
                 />
             </Link>
-            <div className='header__links'>
-             <Link to={'/addCottage'} style={{textDecoration: 'none', color:'black'}}>Add Cottage</Link>
-             <Link to={'/addBoat'} style={{textDecoration: 'none', color:'black'}}>Add Boat</Link>
-             <Link to={'/addAdventure'} style={{textDecoration: 'none', color:'black'}}>Add Adventure</Link>
-             <Link to={'/editProfile'} style={{textDecoration: 'none', color:'black'}}>Edit Your Profile</Link>
-             <Link to={'/showEntities'} style={{textDecoration: 'none', color:'black'}}>View Offers</Link>
-             <Link to={'/viewHostEntities'} style={{textDecoration: 'none', color:'black'}}>View your entities</Link>
-
-            </div>
-             
+            
+            {role===null && getUnauthorizedOptions()}
+            {role==="CLIENT" && getClientOptions()}
+            {role==="COTTAGE_OWNER" && getCottageOwnerOptions()}
+            {role==="BOAT_OWNER" && getBoatOwnerOptions()}
+            {role==="INSTRUCTOR" && getInstructorOptions()}
+                 
             <div className='header__right'>
                 {
                     isUserLogged?
@@ -51,21 +106,27 @@ export default function Header(){
                     :
                     <Link to={'/login'} style={{textDecoration: 'none', color:'black'}}>Login</Link>
                 }
-            <ul className="menus">
-                {menuItems.map((menu, index) => {
-                    return <MenuItems items={menu} key={index} />;
-                })}
-            </ul>
-                <ExpandMoreIcon/>
-                <Link to={'/clientRegistration'} style={{textDecoration: 'none', color:'black'}}>
-                    <Avatar/>
-                </Link>
+            {!isUserLogged &&
+                <div className='header__registrations'>
+                    <ul className="menus">
+                        {menuItems.map((menu, index) => {
+                            return <MenuItems items={menu} key={index} />;
+                        })}
+                    </ul>
+                    <ExpandMoreIcon/>
+                    <Link to={'/clientRegistration'} style={{textDecoration: 'none', color:'black'}}>
+                        <Avatar/>
+                    </Link>
+                </div>
+            }
                 
             </div>
         </div>
 
     )
 }
+
+
 
 export const menuItems = [
     {

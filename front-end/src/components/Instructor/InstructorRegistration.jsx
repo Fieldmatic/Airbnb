10e76e25ccd0d16f216db82c529ba025103cfa40
banzg00 <63750104+banzg00@users.noreply.github.com>
2022-05-24 22:54:16 +0200

    Izmjena dizajna forme za registraciju

--- front-end/src/components/Instructor/InstructorRegistration.jsx
+++ front-end/src/components/Instructor/InstructorRegistration.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./Instructor.css";
-import InstructorService from "../../services/InstructorService"
+import LoginRegisterService from "../../services/LoginRegisterService"
 import { Dropzone, FileItem, FullScreenPreview } from "@dropzone-ui/react";
-import { Navigate } from "react-router-dom";
 import Header from "../../Header";
+import { useNavigate } from "react-router-dom";
+import { TextField } from '@mui/material';
+import muiStyles from '../utils/muiStyles';
+import PhoneInput from 'react-phone-number-input'
+import 'react-phone-number-input/style.css'
+
 
 export default function InstructorRegistration() {
     const [formData, setFormData] = React.useState({
@@ -25,60 +30,15 @@ export default function InstructorRegistration() {
         profilePhoto: ""
     });
 
-    const [redirect, setRedirect] = React.useState("");
-    
-    function handleChange(event) {
-        const {name, value} = event.target;
-        setFormData(prevFormData => ({
-            ...prevFormData,
-            [name]: value
-        }));
-    }
-
-    function handleAddressChange(event) {
-        const {name, value} = event.target
-        const address = formData.address
-        setFormData(prevFormData => {
-            return {
-                ...prevFormData,
-                address: {
-                    ...address,
-                    [name]:value
-                }
-            }
-        })
-      }
-    
-    function handleSubmit(event) {
-        event.preventDefault();
-        if (formData.password !== formData.confirmPassword) {
-            alert("Passwords doesn't match!")
-            return;
-        }
-        let data = new FormData()
-        const instructorJson = instructorToJson();
-        data.append("instructor", instructorJson)
-        files.map((file) => {
-            data.append("files", file.file)
-        })
-        InstructorService.addInstructor(data)
-        .then(response => {
-            if (response.data === "NC"){
-                alert("Username already exist!")
-            } else {
-                alert("Success!")
-                setRedirect("/")
-            }
-        });
-    }
+    let navigate = useNavigate();
 
     const [files, setFiles] = React.useState([]);
 
     const [imageSrc, setImageSrc] = React.useState(undefined);
 
-    const updateFiles = (incommingFile) => {
-        console.log("incomming file", incommingFile);
-        setFiles(incommingFile);
+    const updateFiles = (incommingFiles) => {
+        console.log("incomming files", incommingFiles);
+        setFiles(incommingFiles);
     };
 
     const onDelete = (id) => {
@@ -93,170 +53,260 @@ export default function InstructorRegistration() {
         console.log("list cleaned", files);
     };
 
-    // function validateForm() {
-    //     setValidFrom(true);
-    //     validateName();
-    //     validateAddress();
-    //     validateCapacity();
-    //     validateHourlyRate();
-    // }
+    function handleChange(event) {
+        const {name, value} = event.target
+        setFormData(prevFormData => {
+        return {
+            ...prevFormData,
+            [name]: value
+        }
+        })
+        console.log(formData)
+    }
+
+    function handleAddressChange(event) {
+        const {name, value} = event.target
+        const address = formData.address
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                address: {
+                        ...address,
+                        [name]:value
+                        }
+            }
+        })
+    }
     
-    function instructorToJson() {
-        let formDataCopy = { ...formData };
-        const json = JSON.stringify(formDataCopy);
+    function handleSubmit(event){
+        event.preventDefault()
+        if (formData.password !== formData.passwordRetype) {
+            alert("Passwords aren't matching")
+            return
+        }
+        let data = new FormData()
+        const instructorJson = getInstructorJson();
+        data.append("instructor", instructorJson)
+        files.map((file) => {
+            data.append("files", file.file)
+        })
+        LoginRegisterService.registerInstructor(data).then(response => {
+            if (response.status === 200) alert("Success");
+            navigate("/");
+        });
+    }
+
+    function getInstructorJson() {
+        const json = JSON.stringify(formData);
         const instructorJson = new Blob([json], {
             type: 'application/json'
         });
         return instructorJson;
     }
-    if (redirect){
-        return (
-            <Navigate to={redirect}/>
-        )
-    }
+
     return (
         <div>
             <Header />
             <div className="form-container">
                 <form className="form" onSubmit={handleSubmit}>
-                    <h2 className="form--title">Instructor Registration</h2>
-                    <input 
-                        type="text"
-                        placeholder="Username"
-                        className="form--input"
-                        name="username"
-                        onChange={handleChange}
-                        value={formData.username}
-                    />
-                    <input 
-                        type="password"
-                        placeholder="Password"
-                        className="form--input"
-                        name="password"
-                        onChange={handleChange}
-                        value={formData.password}
-                    />
-                    <input 
-                        type="password"
-                        placeholder="Confirm password"
-                        className="form--input"
-                        name="confirmPassword"
-                        onChange={handleChange}
-                        value={formData.confirmPassword}
-                    />
-                    <input 
-                        type="text"
-                        placeholder="Name"
-                        className="form--input"
-                        name="name"
-                        onChange={handleChange}
-                        value={formData.name}
-                    />
-                    <input 
-                        type="text"
-                        placeholder="Surname"
-                        className="form--input"
-                        name="surname"
-                        onChange={handleChange}
-                        value={formData.surname}
-                    />
-                    <input 
-                        type="email"
-                        placeholder="E-mail"
-                        className="form--input"
-                        name="email"
-                        onChange={handleChange}
-                        value={formData.email}
-                    />
-                    <input 
+                <h1 className='form--header'>Instructor Registration</h1>
+                <div className='form--pair'>
+                <TextField
+                    sx={muiStyles.style} 
+                    label = "Name"
+                    variant='outlined'
+                    id="standard-basic"
+                    className="form--input"
+                    type = "text"           
+                    onChange = {handleChange}
+                    name = "name"
+                    value = {formData.name}   
+                />
+                <TextField
+                    sx={muiStyles.style} 
+                    variant='outlined'
+                    className="form--input"
+                    type = "text"
+                    label = "Surname"
+                    onChange = {handleChange}
+                    name = "surname"
+                    value = {formData.surname}   
+                />
+                </div>
+                <div className='form--pair'>
+                    <TextField
+                        sx={muiStyles.style} 
+                        label = "Country"
+                        variant='outlined'
+                        id="standard-basic"
                         className="form--input"
                         type = "text"
-                        placeholder = "State"
                         onChange = {handleAddressChange}
                         name = "state"
                         value = {formData.address.state}          
                     />
-                    <input 
+                    <TextField
+                        sx={muiStyles.style} 
+                        label = "Zip"
+                        variant='outlined'
+                        id="standard-basic"
                         className="form--input"
                         type = "text"
-                        placeholder = "City"
-                        onChange = {handleAddressChange}
-                        name = "city"
-                        value = {formData.address.city}          
-                    />
-                    <input 
-                        className="form--input"
-                        type = "text"
-                        placeholder = "Zip"
                         onChange = {handleAddressChange}
                         name = "zipCode"
                         value = {formData.address.zipCode}          
                     />
-                    <input 
+                </div>
+                <div className='form--pair'>
+                    <TextField
+                        sx={muiStyles.style} 
+                        label = "City"
+                        variant='outlined'
+                        id="standard-basic"
                         className="form--input"
                         type = "text"
-                        placeholder = "Street"
+                        onChange = {handleAddressChange}
+                        name = "city"
+                        value = {formData.address.city}          
+                    />
+                    <TextField
+                        sx={muiStyles.style} 
+                        label = "Street"
+                        variant='outlined'
+                        id="standard-basic"
+                        className="form--input"
+                        type = "text"
                         onChange = {handleAddressChange}
                         name = "street"
                         value = {formData.address.street}          
                     />
-                    <input 
-                        type="text" 
-                        placeholder="Phone"
+                </div>
+                <div className='form--pair'>
+                 <TextField
+                        sx={muiStyles.style} 
+                        label = "Username"
+                        variant='outlined'
+                        id="standard-basic"
                         className="form--input"
-                        name="phone"
-                        onChange={handleChange}
-                        value={formData.phone}
+                        type = "text"
+                        placeholder = "Username"
+                        onChange = {handleChange}
+                        name = "username"
+                        value = {formData.username}   
                     />
-                    <textarea 
-                        placeholder="Biography"
-                        className="form--input-area"
-                        name="biography"
-                        onChange={handleChange}
-                        value={formData.biography}
+                    <TextField
+                        sx={muiStyles.style} 
+                        label = "Email"
+                        variant='outlined'
+                        id="standard-basic"
+                        className="form--input"
+                        type = "text"
+                        onChange = {handleChange}
+                        name = "email"
+                        value = {formData.email}   
                     />
-                    <textarea 
-                        placeholder="Why you want to create an account?"
-                        className="form--input-area"
-                        name="registrationExplanation"
-                        onChange={handleChange}
-                        value={formData.registrationExplanation}
+                </div>
+                <div className='form--pair'>
+                    <TextField
+                        sx={muiStyles.style} 
+                        label = "Password"
+                        variant='outlined'
+                        id="standard-basic"
+                        className="form--input"
+                        type = "password"
+                        onChange = {handleChange}
+                        name = "password"
+                        value = {formData.password}   
                     />
-                    <Dropzone
-                        style={{ minWidth: "95%", margin:"20px", fontSize:"18px" }}
-                        onChange={updateFiles}
-                        minHeight="20vh"
-                        onClean={handleClean}
-                        value={files}
-                        maxFiles={1}
-                        header={true}
-                        maxFileSize={5000000}
-                        >
-                        {files.map((file) => (
-                            <FileItem
-                            {...file}
-                            key={file.id}
-                            onDelete={onDelete}
-                            onSee={handleSee}
-                            resultOnTooltip
-                            preview
-                            info
-                            hd
-                            />
-                        ))}
-                        <FullScreenPreview
-                            imgSource={imageSrc}
-                            openImage={imageSrc}
-                            onClose={(e) => handleSee(undefined)}
-                        />
-                    </Dropzone>
-                    <button
-                        className="form--submit"
-                    >
-                        Submit
-                    </button>
-                </form>
-            </div>
+                    <TextField
+                        sx={muiStyles.style} 
+                        label = "Retype password"
+                        variant='outlined'
+                        id="standard-basic"
+                        className="form--input"
+                        type = "password"
+                        onChange = {handleChange}
+                        name = "passwordRetype"
+                        value = {formData.passwordRetype}   
+                    />
+                </div>
+                <div className='form--pair'>
+                    <PhoneInput
+                        className="form--phoneInput"
+                        onChange = {(value) => {
+                            setFormData(prevFormData => {
+                            return {
+                                ...prevFormData,
+                                phoneNumber:value
+                            }
+                            });
+                        }}                  
+                        placeholder = "Phone number"
+                        name = "phoneNumber"
+                        value = {formData.phoneNumber}   
+                    />
+                </div>
+                <div className='form--pair'>
+                <TextField
+                    id="outlined-multiline-flexible"
+                    label="Registration explanation"
+                    className="form--input-area"
+                    sx = {muiStyles.style}
+                    multiline
+                    maxRows={6}
+                    name = "registrationExplanation"
+                    value={formData.registrationExplanation}
+                    onChange={handleChange}
+                />
+                </div>
+                <div className="form--pair">
+                <TextField
+                    id="outlined-multiline-flexible"
+                    label="Biography"
+                    className="form--input-area"
+                    sx = {muiStyles.style}
+                    multiline
+                    maxRows={6}
+                    name = "biography"
+                    value={formData.biography}
+                    onChange={handleChange}
+                />
+                </div>
+                <div className='form--pair'>
+                <Dropzone
+                    style={{ minWidth: "100%", fontSize:"18px" }}
+                    onChange={updateFiles}
+                    minHeight="20vh"
+                    onClean={handleClean}
+                    value={files}
+                    maxFiles={1}
+                    header={true}
+                    maxFileSize={5000000}
+                >
+                    {files.map((file) => (
+                    <FileItem
+                        {...file}
+                        key={file.id}
+                        onDelete={onDelete}
+                        onSee={handleSee}
+                        resultOnTooltip
+                        preview
+                        info
+                        hd
+                    />
+                    ))}
+                    <FullScreenPreview
+                    imgSource={imageSrc}
+                    openImage={imageSrc}
+                    onClose={(e) => handleSee(undefined)}
+                    />
+                </Dropzone>
+                </div>
+                <div className='form--pair'>
+                <button className="form--submit">Submit</button>  
+                </div>
+            </form>
         </div>
+    </div>
     )
 }
--- front-end/src/services/LoginRegisterService.js
+++ front-end/src/services/LoginRegisterService.js
@@ -26,6 +26,10 @@ class LoginRegisterService {
         return axios.post (LOGIN_REGISTER_BASED_REST_API_URL + "/clientRegistration", formData)
     }
 
+    registerInstructor(formData) {
+        return axios.post (LOGIN_REGISTER_BASED_REST_API_URL + "/instructorRegistration", formData)
+    }
+
 }
 
 

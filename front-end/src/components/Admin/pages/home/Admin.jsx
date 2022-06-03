a5762fb9e520bca20fbb2d33c022d0e154d11381
banzg00 <63750104+banzg00@users.noreply.github.com>
2022-06-03 18:32:34 +0200

    Dodavanje widgeta

--- front-end/src/components/Admin/pages/home/Admin.jsx
+++ front-end/src/components/Admin/pages/home/Admin.jsx
@@ -41,6 +41,9 @@ export default function Admin() {
             <AdminSidebar />
             <div className="homeContainer">
                 <AdminNavbar />
+                <div className="widgets">
+                    
+                </div>
             </div>
         </div>
     )

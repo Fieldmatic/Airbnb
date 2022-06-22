0037c7a16aa8a0a35294e389bb7f88eccae1a782
Fieldmatic <milicftn@gmail.com>
2022-06-22 21:41:11 +0200

    data load

--- Back-end/src/main/java/mrsisa/project/ProjectApplication.java
+++ Back-end/src/main/java/mrsisa/project/ProjectApplication.java
@@ -1,29 +1,48 @@
 package mrsisa.project;
 
+import mrsisa.project.model.*;
+import mrsisa.project.repository.ClientRepository;
+import mrsisa.project.repository.CottageRepository;
 import mrsisa.project.service.AdminService;
+import mrsisa.project.service.CottageService;
+import mrsisa.project.service.MockupService;
+import mrsisa.project.service.PeriodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.io.IOException;
 
 @SpringBootApplication
 @EnableAsync
+@EnableCaching
 public class ProjectApplication implements CommandLineRunner {
 
 	@Autowired
 	private AdminService adminService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
-	}
+	@Autowired
+	private CottageRepository cottageRepository;
+
+	@Autowired
+	private CottageService cottageService;
+
+	@Autowired
+	private ClientRepository clientRepository;
+
+	@Autowired
+	private PeriodService periodService;
+
+	@Autowired
+	private MockupService mockupService;
 
 	@Override
 	public void run(String... args) throws IOException {
 		adminService.createFirstAdmin();
-/*		Client client1 = mockupService.createClient1();
+		Client client1 = mockupService.createClient1();
 		Cottage cottage1 = mockupService.createCottage1();
 		mockupService.subscribeClientOnCottage(clientRepository.findClientByUsernameWithSubscriptions(client1.getUsername()), cottage1);
 		CottageOwner cottageOwner = mockupService.createCottageOwner1();
@@ -36,8 +55,12 @@ public class ProjectApplication implements CommandLineRunner {
 		Boat boat1 = mockupService.createBoat1();
 		Adventure adventure1 = mockupService.createAdventure1();
 		mockupService.addPeriodToBookable(boat1.getId());
-		mockupService.addPeriodToBookable(adventure1.getId());*/
+		mockupService.addPeriodToBookable(adventure1.getId());
 
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
 	}
 
 }
